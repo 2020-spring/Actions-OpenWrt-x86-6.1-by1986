668bae47f1f60e9c2b22a85eb39fa68469b7acef
2020-spring <61011844+2020-spring@users.noreply.github.com>
2024-08-26 20:13:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,14 +13,6 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.4.1/g' package/base-files/files/bin/config_generate
 sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=6.1/g' target/linux/x86/Makefile
-
-
-# 修改golang源码以编译xray1.8.8+版本
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
-sed -i '/-linkmode external \\/d' feeds/packages/lang/golang/golang-package.mk
-
-
 sed -i '/-linkmode external \\/d' feeds/packages/lang/golang/golang-package.mk
 #sed -i 's/OpenWrt/CncTion/g' package/base-files/files/bin/config_generate
 #sed -i "s/.*PKG_VERSION:=.*/PKG_VERSION:=4.3.9_v1.2.14/" package/lean/qBittorrent-static/Makefile
